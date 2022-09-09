3d591a9cbe9aa67f3cc590f34a58fd4af62efc47
karabo <karaboelijahkoka26@gmail.com>
2022-09-09 15:16:30 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: printing zero to nine
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar("\n");
+	return (0);
+}
