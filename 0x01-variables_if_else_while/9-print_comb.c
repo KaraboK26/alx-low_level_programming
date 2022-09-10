52a41e8502964c8556c8a2f3d7349244912b4ce9
karabo <karaboelijahkoka26@gmail.com>
2022-09-09 17:29:20 -0700

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(",");
+			putchar(" ");
+		}
+	}
+	putchar("\n");
+	return (0);
+}
