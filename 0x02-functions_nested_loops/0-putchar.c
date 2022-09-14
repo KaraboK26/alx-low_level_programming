865cc9498f12374cf7fc98931e5062381012e82b
karabo <karaboelijahkoka26@gmail.com>
2022-09-14 05:11:24 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints_putchar
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar");
+	printf('\n');
+	return (0);
+}
